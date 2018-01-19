8a13ce0f502ba5a3304f900243cfbce52ba95d97
Livio Brunner <livio.brunner.lb1@gmail.com>
2018-01-19 10:44:40 +0100

    Add aufgabe 1

--- Pruefung_Praktisch_Musterloesung/Controllers/Lab1Controller.cs
+++ Pruefung_Praktisch_Musterloesung/Controllers/Lab1Controller.cs
@@ -11,18 +11,46 @@ namespace Pruefung_Praktisch_Musterloesung.Controllers
     {
         /**
          * 
-         * ANTWORTEN BITTE HIER
+         * Overview:
+         * 
+         * Beim "type" parameter-query-string kann man eigene Pfade eingeben und somit
+         * sensible daten anfordern
+         * 
+         * Beispielurl:
+         * http://localhost:8080/lab1/index/?type=../../App_Data/
+         * 
+         * Es würde den path /Content/images/../../App_Data mit
+         * Directory.GetFiles()-auflisten. Alle files werden im img-src
+         * angezeigt.
+         * 
+         * ------
+         * 
+         * Detail:
+         * Hier ebenfalls kann man den Ordner und sogar noch 
+         * das file bestimmen mit dem type und file query parameter
+         * 
+         * Beispielurl:
+         * http://localhost:8080/lab1/index/?type=../../App_Data&file=lab3.mdf
+         * 
+         * Es würde den Path Content/images/../../App_Data/lab3.mdf auflösen
+         * welches die url der Datebank in den img-src anzeigen würde
          * 
          * */
-
+        List<string> types = new List<string> { "bears", "elephants", "lions" };
 
         public ActionResult Index()
         {
-            var type = Request.QueryString["type"];
+            var type = Request.QueryString["type"].ToLower();
+            
 
             if (string.IsNullOrEmpty(type))
             {
-                type = "lions";                
+                type = "lions";
+            }
+
+            if (!types.Contains(type))
+            {
+                throw new ApplicationException("Sie dürfen diesen Typ nicht angeben");
             }
 
             var path = "~/Content/images/" + type;
@@ -31,10 +59,10 @@ namespace Pruefung_Praktisch_Musterloesung.Controllers
 
             if (Directory.Exists(Server.MapPath(path)))
             {
-                var scheme = Request.Url.Scheme; 
-                var host = Request.Url.Host; 
+                var scheme = Request.Url.Scheme;
+                var host = Request.Url.Host;
                 var port = Request.Url.Port;
-                
+
                 string[] fileEntries = Directory.GetFiles(Server.MapPath(path));
                 foreach (var filepath in fileEntries)
                 {
@@ -49,14 +77,14 @@ namespace Pruefung_Praktisch_Musterloesung.Controllers
                     fileUriList.Add(urilistelement);
                 }
             }
-            
+
             return View(fileUriList);
         }
 
         public ActionResult Detail()
         {
             var file = Request.QueryString["file"];
-            var type = Request.QueryString["type"];
+            var type = Request.QueryString["type"].ToLower();
 
             if (string.IsNullOrEmpty(file))
             {
@@ -65,6 +93,18 @@ namespace Pruefung_Praktisch_Musterloesung.Controllers
             if (string.IsNullOrEmpty(type))
             {
                 file = "lions";
+            }
+
+            if (!types.Contains(type))
+            {
+                throw new ApplicationException("Sie dürfen diesen Typ nicht angeben");
+            }
+
+            var folderpath = "/Content/images/" + type;
+
+            if(!Directory.GetFiles(folderpath).Contains(file))
+            {
+                throw new ApplicationException("Das File existiert nicht");
             }
 
             var relpath = "~/Content/images/" + type + "/" + file;
@@ -89,7 +129,7 @@ namespace Pruefung_Praktisch_Musterloesung.Controllers
 
                 fileUriItem.Add(urilistelement);
             }
-            
+
             return View(fileUriItem);
         }
     }
